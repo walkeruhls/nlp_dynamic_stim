472518f9319a0bb6b2a18836d59b1bc4f1d4e681
Walker Uhls <walkeruhls@gmail.com>
2023-02-23 19:08:23 -0500

    Built and deployed latest build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
